41889680c9bb9e07c4823eecace5be50cba49468
farqnyy8 <faruqhammed8@gmail.com>
2021-11-28 22:45:06 -0600

    added func code - yet to incorporate

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import multer from "multer";
 import cors from "cors";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
+import utils from "./utils";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -42,6 +43,46 @@ app.post("/upload", function (req, res) {
     }
     return res.status(200).send(req.file);
   });
+});
+
+app.get("/generateReports", function (req, res) {
+  var files = await fsp.readdir(path.join(__dirname, 'transcripts'));
+
+  if (!files) {
+    res.send(500).send("error processing files");
+  }
+
+  for (var i = 0; i < files.length; i++) {
+    utils.getJsonFromPdf(
+      path.join(__dirname, 'transcripts', files[i]),
+      path.join(__dirname, 'reports')
+    );
+  }
+
+  res.send("processing files");
+})
+
+app.get('/getReport', async (req, res) => {
+
+  let reports = [];
+  var files = await fsp.readdir(path.join(__dirname, 'reports'));
+
+  for (var i = 0; i < files.length; i++) {
+    var data = await fsp.readFile(path.join(__dirname, 'reports', files[i]));
+    reports.push(utils.generateSingleReport(JSON.parse(data)));
+  }
+
+  res.json({ reports: reports });
+});
+
+app.get("/getReport/:id", async (req, res) => {
+  try {
+    var data = await fsp.readFile(path.join(__dirname, 'reports', req.params.id + ".json"));
+    res.json({ report: JSON.parse(data) });
+  }
+  catch (err) {
+    res.status(404).send("report not generated");
+  }
 });
 
 const PORT = 8000;
